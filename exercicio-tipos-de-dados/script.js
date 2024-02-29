52feee50394e04a789b48366c83bdaa2144d5fcd
Lucas <67424170+LucasCatuyama@users.noreply.github.com>
2024-02-29 08:57:58 +0000

    introduction data type

---
+++ exercicio-tipos-de-dados/script.js
@@ -0,0 +1,26 @@
+//Declare uma variável contendo uma string
+let frase = 'Isso é uma string.'
+console.log(frase);
+
+//Declare uma variável contendo um número dentro de uma string
+let num = '10';
+console.log(num);
+
+//Declare uma variável com a sua idade
+let idade = 23;
+console.log(idade);
+
+//Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+let nome = 'Lucas';
+let sobreNome = 'Catuyama'
+let nomeCompleto = nome + sobreNome;
+let nomeCompletoTemplate = `${nome} ${sobreNome}`
+console.log(nomeCompletoTemplate);
+console.log(nomeCompleto);
+
+//Coloque a seguinte frase em uma variável: It's time
+let exemplo = "It's time";
+console.log(exemplo);
+
+//Verifique o tipo da variáel que contém o seu nome
+console.log(typeof nomeCompleto);
---
+++ exercicio/script.js
@@ -0,0 +1,15 @@
+let nome = 'Lucas Catuyama';
+let idade = 23;
+let comidaFavorita;
+comidaFavorita = 'Ramen';
+let primeira,
+    segunda,
+    terceira,
+    quarta,
+    quinta;
+
+console.log(nome);
+console.log(idade);
+console.log(comidaFavorita);
+console.log(nome, idade, comidaFavorita);
+
